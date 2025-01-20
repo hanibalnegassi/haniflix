736ce0327f427325c34c0b27e85ab7ca39578867
NotAbdullah87 <yehtohdekho@gmail.com>
2025-01-20 22:57:56 +0500

    Updated SignUp form

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -6,7 +6,10 @@ import '../../Assets/css/styles.scss';
 import { addClassNames } from '../../store/utils/functions';
 import styles from './register.module.scss';
 import { Link } from 'react-router-dom';
-
+import OutlookLogo from "../../Assets/Images/Outlook-logo.png"
+import AOL from "../../Assets/Images/AOL-logo.png";
+import YahooLogo from "../../Assets/Images/Yahoo-logo.png";
+import OutlookBlueLogo from "../../Assets/Images/Outlook-blue-logo.png";
 const api_url = import.meta.env.VITE_APP_API_URL;
 
 enum STEP {
@@ -14,10 +17,23 @@ enum STEP {
   BILLING,
 }
 
+
+
+const providerLogos = {
+  google: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png",
+  yahoo: YahooLogo,
+  outlook: OutlookBlueLogo,
+  aol: AOL
+};
+
+
 const Register = () => {
   const navigate = useNavigate();
-
+  const [showPopup, setShowPopup] = useState(null);
   const [step, setStep] = useState(STEP.SIGNUP);
+  const [formStep, setFormStep] = useState(1);
+  const [showPassword, setShowPassword] = useState(false);
+  const [errors, setErrors] = useState({});
 
   // sign up
 
@@ -211,11 +227,14 @@ const Register = () => {
     const value = event.target.value;
     setEmail(value);
     validateEmail(value);
+    setErrors({ ...errors, email: '' });
+
   };
 
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setUsername(value);
+    setErrors({ ...errors, username: '' });
     validateUsername(value);
   };
 
@@ -223,6 +242,8 @@ const Register = () => {
     const value = event.target.value;
     setPassword(value);
     validatePassword(value);
+    setErrors({ ...errors, password: '' });
+
   };
 
   const handleRepeatPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -375,12 +396,151 @@ const Register = () => {
       });
   };
 
+  // popUp Controller 
+  const validateForm = () => {
+
+    const newErrors = {};
+    if (validateUsername(username)) {
+        newErrors.username = 'Username is required';
+    }
+    if (!email) {
+        newErrors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+        newErrors.email = 'Email address is invalid';
+    }
+    if (!password && formStep === 2) {
+        newErrors.password = 'Password is required';
+    }
+
+    console.log(errors)
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+};
+
+const handleNext = () => {
+
+    if (formStep === 1) {
+      console.log("here")
+        if (validateForm()) {
+            setFormStep(2);
+        }
+    } else {
+        if (validateForm()) {
+            // Handle form submission
+            console.log('Username:', username);
+            console.log('Email:', email);
+            console.log('Password:', password);
+            setShowPopup(null);
+        }
+    }
+
+
+  
+};
+
+const openPopup = (provider) => {
+    setShowPopup(provider);
+    setFormStep(1);
+};
+
+const closePopup = () => {
+    setShowPopup(null);
+};
+
+const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+};
+
   const signUpForm = (
     <>
-      <h2 className='text-white font-[500] text-[42px] m-[auto] w-[fit-content] gradient-text'>
-        Sign Up
-      </h2>
-      <div style={{ maxWidth: '450px', width: '100%' }}>
+        <h2 className='text-white font-[500] text-[42px] m-[auto] w-[fit-content] gradient-text'>
+                Sign Up
+            </h2>
+
+            <div style={{ maxWidth: '450px', width: '100%' }}>
+                <div className={styles['OutWrapper']}>
+                    <div className='flex flex-col gap-y-2 mt-12'>
+                        {/* Google Button */}
+                        <button className={styles['google-button']} onClick={() => openPopup('google')}>
+                            <div className='bg-white h-full rounded-xl'>
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png" alt="Google logo" className={styles['google-icon']} />
+                            </div>
+                            <span className='ml-6'>Sign up with Google</span>
+                        </button>
+
+                        {/* Yahoo Button */}
+                        <button className={styles['yahoo-button']} onClick={() => openPopup('yahoo')}>
+                            <div className='h-full rounded-md ml-1 mt-1'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} aria-label="Yahoo!" role="img" viewBox="0 0 512 512" fill="#000000">
+                                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <rect width="512" height="512" rx="15%" fill="#5f01d1"></rect>
+                                        <g fill="#ffffff">
+                                            <path d="M203 404h-62l25-59-69-165h63l37 95 37-95h62m58 76h-69l62-148h69"></path>
+                                            <circle cx="303" cy="308" r="38"></circle>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </div>
+                            <span className='ml-7'>Sign up with Yahoo</span>
+                        </button>
+
+                        {/* Outlook Button */}
+                        <button className={styles['outlook-button']} onClick={() => openPopup('outlook')}>
+                            <div className='h-full rounded-md ml-1 mt-1'>
+                                <img src={OutlookLogo} alt="Outlook logo" className={styles['outlook-icon']} />
+                            </div>
+                            <span className='ml-7'>Sign up with Outlook</span>
+                        </button>
+
+                        {/* AOL Button */}
+                        <button className={styles['aol-button']} onClick={() => openPopup('aol')}>
+                            <div className='h-full rounded-md ml-1 mt-1'>
+                                <img src={AOL} alt="AOL logo" className={styles['aol-icon']} />
+                            </div>
+                            <span style={{ color: "black" }} className='ml-7 text-black font-medium hover:text-white'>Sign up with AOL</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+       
+            {showPopup && (
+                <div className={styles['popup-overlay']}>
+                    <div className={styles['popup']}>
+                        <button className={styles['close-button']} onClick={closePopup}>×</button>
+                        <div className={styles['popup-header']}>
+                            <img src={providerLogos[showPopup]} alt={`${showPopup} logo`} className={styles['provider-logo']} />
+                            <h3>{`Sign up with ${showPopup.charAt(0).toUpperCase() + showPopup.slice(1)}`}</h3>
+                        </div>
+                        {formStep === 1 && (
+                            <div>
+                                <input type="text" name='username' onChange={handleUsernameChange} value={username} placeholder="Username" className={styles['input']} />
+                                {errors.username && <p className={styles['error']}>{errors.username}</p>}
+                                <input type="email" name='email' onChange={handleEmailChange} value={email} placeholder="Email" className={styles['input']} />
+                                {errors.email && <p className={styles['error']}>{errors.email}</p>}
+                                <button onClick={handleNext} className={styles['next-button']}>Next</button>
+                            </div>
+                        )}
+                        {formStep === 2 && (
+                            <div>
+                                <div className={styles['password-input']}>
+                                    <input type={showPassword ? 'text' : 'password'} name='password' onChange={handlePasswordChange} value={password} placeholder="Password" className={styles['input']} />
+                                    <button type="button" onClick={togglePasswordVisibility} className={styles['toggle-password']}>
+                                        {showPassword ? <svg viewBox="0 0 24 24" width={25} fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.5" d="M2 12C2 13.6394 2.42496 14.1915 3.27489 15.2957C4.97196 17.5004 7.81811 20 12 20C16.1819 20 19.028 17.5004 20.7251 15.2957C21.575 14.1915 22 13.6394 22 12C22 10.3606 21.575 9.80853 20.7251 8.70433C19.028 6.49956 16.1819 4 12 4C7.81811 4 4.97196 6.49956 3.27489 8.70433C2.42496 9.80853 2 10.3606 2 12Z" fill="#1C274C"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M8.25 12C8.25 9.92893 9.92893 8.25 12 8.25C14.0711 8.25 15.75 9.92893 15.75 12C15.75 14.0711 14.0711 15.75 12 15.75C9.92893 15.75 8.25 14.0711 8.25 12ZM9.75 12C9.75 10.7574 10.7574 9.75 12 9.75C13.2426 9.75 14.25 10.7574 14.25 12C14.25 13.2426 13.2426 14.25 12 14.25C10.7574 14.25 9.75 13.2426 9.75 12Z" fill="#1C274C"></path> </g></svg> : <svg viewBox="0 0 24 24" width={25} fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2.68936 6.70456C2.52619 6.32384 2.08528 6.14747 1.70456 6.31064C1.32384 6.47381 1.14747 6.91472 1.31064 7.29544L2.68936 6.70456ZM15.5872 13.3287L15.3125 12.6308L15.5872 13.3287ZM9.04145 13.7377C9.26736 13.3906 9.16904 12.926 8.82185 12.7001C8.47466 12.4742 8.01008 12.5725 7.78417 12.9197L9.04145 13.7377ZM6.37136 15.091C6.14545 15.4381 6.24377 15.9027 6.59096 16.1286C6.93815 16.3545 7.40273 16.2562 7.62864 15.909L6.37136 15.091ZM22.6894 7.29544C22.8525 6.91472 22.6762 6.47381 22.2954 6.31064C21.9147 6.14747 21.4738 6.32384 21.3106 6.70456L22.6894 7.29544ZM19 11.1288L18.4867 10.582V10.582L19 11.1288ZM19.9697 13.1592C20.2626 13.4521 20.7374 13.4521 21.0303 13.1592C21.3232 12.8663 21.3232 12.3914 21.0303 12.0985L19.9697 13.1592ZM11.25 16.5C11.25 16.9142 11.5858 17.25 12 17.25C12.4142 17.25 12.75 16.9142 12.75 16.5H11.25ZM16.3714 15.909C16.5973 16.2562 17.0619 16.3545 17.409 16.1286C17.7562 15.9027 17.8545 15.4381 17.6286 15.091L16.3714 15.909ZM5.53033 11.6592C5.82322 11.3663 5.82322 10.8914 5.53033 10.5985C5.23744 10.3056 4.76256 10.3056 4.46967 10.5985L5.53033 11.6592ZM2.96967 12.0985C2.67678 12.3914 2.67678 12.8663 2.96967 13.1592C3.26256 13.4521 3.73744 13.4521 4.03033 13.1592L2.96967 12.0985ZM12 13.25C8.77611 13.25 6.46133 11.6446 4.9246 9.98966C4.15645 9.16243 3.59325 8.33284 3.22259 7.71014C3.03769 7.3995 2.90187 7.14232 2.8134 6.96537C2.76919 6.87696 2.73689 6.80875 2.71627 6.76411C2.70597 6.7418 2.69859 6.7254 2.69411 6.71533C2.69187 6.7103 2.69036 6.70684 2.68957 6.70503C2.68917 6.70413 2.68896 6.70363 2.68892 6.70355C2.68891 6.70351 2.68893 6.70357 2.68901 6.70374C2.68904 6.70382 2.68913 6.70403 2.68915 6.70407C2.68925 6.7043 2.68936 6.70456 2 7C1.31064 7.29544 1.31077 7.29575 1.31092 7.29609C1.31098 7.29624 1.31114 7.2966 1.31127 7.2969C1.31152 7.29749 1.31183 7.2982 1.31218 7.299C1.31287 7.30062 1.31376 7.30266 1.31483 7.30512C1.31698 7.31003 1.31988 7.31662 1.32353 7.32483C1.33083 7.34125 1.34115 7.36415 1.35453 7.39311C1.38127 7.45102 1.42026 7.5332 1.47176 7.63619C1.57469 7.84206 1.72794 8.13175 1.93366 8.47736C2.34425 9.16716 2.96855 10.0876 3.8254 11.0103C5.53867 12.8554 8.22389 14.75 12 14.75V13.25ZM15.3125 12.6308C14.3421 13.0128 13.2417 13.25 12 13.25V14.75C13.4382 14.75 14.7246 14.4742 15.8619 14.0266L15.3125 12.6308ZM7.78417 12.9197L6.37136 15.091L7.62864 15.909L9.04145 13.7377L7.78417 12.9197ZM22 7C21.3106 6.70456 21.3107 6.70441 21.3108 6.70427C21.3108 6.70423 21.3108 6.7041 21.3109 6.70402C21.3109 6.70388 21.311 6.70376 21.311 6.70368C21.3111 6.70352 21.3111 6.70349 21.3111 6.7036C21.311 6.7038 21.3107 6.70452 21.3101 6.70576C21.309 6.70823 21.307 6.71275 21.3041 6.71924C21.2983 6.73223 21.2889 6.75309 21.2758 6.78125C21.2495 6.83757 21.2086 6.92295 21.1526 7.03267C21.0406 7.25227 20.869 7.56831 20.6354 7.9432C20.1669 8.69516 19.4563 9.67197 18.4867 10.582L19.5133 11.6757C20.6023 10.6535 21.3917 9.56587 21.9085 8.73646C22.1676 8.32068 22.36 7.9668 22.4889 7.71415C22.5533 7.58775 22.602 7.48643 22.6353 7.41507C22.6519 7.37939 22.6647 7.35118 22.6737 7.33104C22.6782 7.32097 22.6818 7.31292 22.6844 7.30696C22.6857 7.30398 22.6867 7.30153 22.6876 7.2996C22.688 7.29864 22.6883 7.29781 22.6886 7.29712C22.6888 7.29677 22.6889 7.29646 22.689 7.29618C22.6891 7.29604 22.6892 7.29585 22.6892 7.29578C22.6893 7.29561 22.6894 7.29544 22 7ZM18.4867 10.582C17.6277 11.3882 16.5739 12.1343 15.3125 12.6308L15.8619 14.0266C17.3355 13.4466 18.5466 12.583 19.5133 11.6757L18.4867 10.582ZM18.4697 11.6592L19.9697 13.1592L21.0303 12.0985L19.5303 10.5985L18.4697 11.6592ZM11.25 14V16.5H12.75V14H11.25ZM14.9586 13.7377L16.3714 15.909L17.6286 15.091L16.2158 12.9197L14.9586 13.7377ZM4.46967 10.5985L2.96967 12.0985L4.03033 13.1592L5.53033 11.6592L4.46967 10.5985Z" fill="#1C274C"></path> </g></svg>}
+                                    </button>
+                                    <p className='mb-2 text-[12px]'>Forgot password ?</p>
+                                </div>
+                                {errors.password && <p className={styles['error']}>{errors.password}</p>}
+                                <button onClick={handleNext} className={styles['next-button']}>Submit</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
+
+      {/* <div style={{ maxWidth: '450px', width: '100%' }}>
         <div className={styles['OutWrapper']}>
           <div className={styles['inputWrapper']}>
             <input
@@ -476,7 +636,7 @@ const Register = () => {
             </Link>
           </span>
         </div>
-      </div>
+      </div> */}
     </>
   );
 
